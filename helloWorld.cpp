d3bfeab6dbf9f14d3865fcac70679db522c9fd47
TVH3ad <TylerTVue16@gmail.com>
2024-08-14 11:54:09 -0700

    Add files via upload

---
+++ helloWorld.cpp
@@ -0,0 +1,14 @@
+// tV 8/14/24
+// helloWorld.cpp
+/*
+ *this is my first C++ program
+ *it only took me an approximate hour and eight minutes to write.
+ *
+ */
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "\nHello, World! " << "another strong" << endl;
+    return 0;
+}
