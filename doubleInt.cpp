a573f6c30181f863e0e41b8db32c0e6cdd031629
TVH3ad <TylerTVue16@gmail.com>
2024-08-14 12:16:15 -0700

    Add files via upload

---
+++ doubleInt.cpp
@@ -0,0 +1,21 @@
+// tV 8/14/24
+//doubleInt.cpp
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int userInteger = 0;
+    int doubleInteger = 0;
+    cout << "Please enter a number: ";
+    cin >> userInteger;
+
+    // Do some processing! Double the user's integer!
+    doubleInteger = userInteger * 2;
+
+    // Output the double int.
+    cout << "\n The Double int is: " << doubleInteger << endl;
+    return 0;
+}
